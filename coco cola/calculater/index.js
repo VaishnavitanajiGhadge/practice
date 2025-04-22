89cac3733131376f84262114a742fcbc13da5528
VaishnavitanajiGhadge <ghadgevaishnavi140@gmail.com>
2025-04-22 18:34:35 +0530

    Add files via upload

---
+++ coco cola/calculater/index.js
@@ -0,0 +1,14 @@
+function addnumber(n){
+   document .getElementById("inputbox").value += n; 
+}
+function calculate(){
+    document .getElementById("inputbox").value =eval(document.getElementById("inputbox").value)
+}
+function clearText(){
+    document .getElementById("inputbox").value ="";
+}
+function deltext(){
+    document .getElementById("inputbox").value ="";
+}
+
+
